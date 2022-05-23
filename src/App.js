22b79b188ecc1ee960addcb195b69b018df294a1
rose <rosedaily1001@gmail.com>
2022-05-24 07:21:22 +0900

    컴포지션, jsx
    - 45강까지

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-// import ExpenseItem from './components/ExpenseItem';
-import Expense from './components/Expense';
+import React from 'react';
+import Expense from './components/Expense/Expense';
 
-function App() {
+const App = () => {
   const expenses = [
     {
       id: 'e1',
@@ -28,29 +28,6 @@ function App() {
       <h2>Let's get started!</h2>
       <Expense items={expenses}/>
     </div>
-    // <div>
-    //   <h2>Let's get started!</h2>
-    //   <ExpenseItem 
-    //     title={expenses[0].title} 
-    //     amount={expenses[0].amount}
-    //     date={expenses[0].date}
-    //   />
-    //   <ExpenseItem 
-    //     title={expenses[1].title} 
-    //     amount={expenses[1].amount}
-    //     date={expenses[1].date}
-    //   />
-    //   <ExpenseItem 
-    //     title={expenses[2].title} 
-    //     amount={expenses[2].amount}
-    //     date={expenses[2].date}
-    //   />
-    //   <ExpenseItem 
-    //     title={expenses[3].title} 
-    //     amount={expenses[3].amount}
-    //     date={expenses[3].date}
-    //   />
-    // </div>
   );
 }
 
